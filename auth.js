55587af16f9a7cd4e6caf20eadd7281a7b4c4d09
ej84 <ej84@njit.edu>
2021-05-05 19:39:22 -0400

    step 3: created auth.js for authentication flow

---
+++ auth.js
@@ -0,0 +1,13 @@
+// auth.js
+
+/**
+ * Required External Modules
+ */
+
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const querystring = require("querystring");
+
+require("dotenv").config();
+
