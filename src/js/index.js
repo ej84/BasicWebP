e9ea731d3691ecd6b9dbdf70ccf7a31eb5eec8d5
ej84 <ej84@njit.edu>
2021-05-05 19:36:31 -0400

    step 2: added auth0 configurations in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,92 @@ import TestJS from "./TestJs.js";
 import ConsoleLogIt from "./ConsoleLogIt.js";
 import getJSON from "./getJSON";
 
+// index.js
+
+/**
+ * Required External Modules
+ */
+
+const express = require("express");
+const path = require("path");
+
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+
+require("dotenv").config();
+
+/**
+ * App Variables
+ */
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+/**
+ * Session Configuration (New!)
+ */
+
+const session = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: false
+};
+
+if (app.get("env") === "production") {
+    // Serve secure cookies, requires HTTPS
+    session.cookie.secure = true;
+}
+
+/**
+ * Passport Configuration (New!)
+ */
+
+const strategy = new Auth0Strategy(
+    {
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        callbackURL: process.env.AUTH0_CALLBACK_URL
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+        /**
+         * Access tokens are used to authorize users to an API
+         * (resource server)
+         * accessToken is the token to call the Auth0 API
+         * or a secured third-party API
+         * extraParams.id_token has the JSON Web Token
+         * profile has all the information from the user
+         */
+        return done(null, profile);
+    }
+);
+
+/**
+ *  App Configuration
+ */
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(expressSession(session));
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+
+
 TestJS();
 getJSON("", function (data) {
     console.log(data);
