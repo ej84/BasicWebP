3cdb1f0103a3ef0a60a628f672a077100f25f118
ej84 <ej84@njit.edu>
2021-05-06 14:51:30 -0400

    step 3: created auth.js for authentication flow

--- auth.js
+++ auth.js
@@ -11,3 +11,36 @@ const querystring = require("querystring");
 
 require("dotenv").config();
 
+/**
+ * Routes Definitions
+ */
+
+router.get("/login", ... );
+
+router.get("/callback", ... );
+
+router.get("/logout", (req, res) => {
+    req.logOut();
+
+    let returnTo = req.protocol + "://" + req.hostname;
+    const port = req.connection.localPort;
+
+    if (port !== undefined && port !== 80 && port !== 443) {
+        returnTo =
+            process.env.NODE_ENV === "production"
+                ? `${returnTo}/`
+                : `${returnTo}:${port}/`;
+    }
+
+    const logoutURL = new URL(
+        `https://${process.env.AUTH0_DOMAIN}/v2/logout`
+    );
+
+    const searchString = querystring.stringify({
+        client_id: process.env.AUTH0_CLIENT_ID,
+        returnTo: returnTo
+    });
+    logoutURL.search = searchString;
+
+    res.redirect(logoutURL);
+});
