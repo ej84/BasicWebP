2ba8c33fbce7e6b0b9ad2c632016159a1c7c8082
ej84 <ej84@njit.edu>
2021-05-06 16:42:05 -0400

    step 7: added another index.js for auth and fixed codes for connections.

--- src/js/index.js
+++ src/js/index.js
@@ -1,136 +1,6 @@
-// index.js
-
-/**
- * Required External Modules
- */
-
-const express = require("express");
-const path = require("path");
-
-const expressSession = require("express-session");
-const passport = require("passport");
-const Auth0Strategy = require("passport-auth0");
-
-require("dotenv").config();
-
-const authRouter = require("./auth");
-
-/**
- * App Variables
- */
-
-const app = express();
-const port = process.env.PORT || "8000";
-
-/**
- * Session Configuration
- */
-
-const session = {
-    secret: process.env.SESSION_SECRET,
-    cookie: {},
-    resave: false,
-    saveUninitialized: false
-};
-
-if (app.get("env") === "production") {
-    // Serve secure cookies, requires HTTPS
-    session.cookie.secure = true;
-}
-
-// index.js
-
-/**
- * Passport Configuration
- */
-
-/**
- * Passport Configuration
- */
-
-const strategy = new Auth0Strategy(
-    {
-        domain: process.env.AUTH0_DOMAIN,
-        clientID: process.env.AUTH0_CLIENT_ID,
-        clientSecret: process.env.AUTH0_CLIENT_SECRET,
-        callbackURL: process.env.AUTH0_CALLBACK_URL
-    },
-    function(accessToken, refreshToken, extraParams, profile, done) {
-        /**
-         * Access tokens are used to authorize users to an API
-         * (resource server)
-         * accessToken is the token to call the Auth0 API
-         * or a secured third-party API
-         * extraParams.id_token has the JSON Web Token
-         * profile has all the information from the user
-         */
-        return done(null, profile);
-    }
-);
-
-/**
- *  App Configuration
- */
-
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use(expressSession(session));
-
-passport.use(strategy);
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser((user, done) => {
-    done(null, user);
-});
-
-passport.deserializeUser((user, done) => {
-    done(null, user);
-});
-
-// Creating custom middleware with Express
-app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.isAuthenticated();
-    next();
-});
-
-// Router mounting
-app.use("/", authRouter);
-
-/**
- * Routes Definitions
- */
-
-const secured = (req, res, next) => {
-    if (req.user) {
-        return next();
-    }
-    req.session.returnTo = req.originalUrl;
-    res.redirect("/login");
-};
-
-app.get("/", (req, res) => {
-    res.render("index", { title: "Home" });
-});
-
-app.get("/user", secured, (req, res, next) => {
-    const { _raw, _json, ...userProfile } = req.user;
-    res.render("user", {
-        title: "Profile",
-        userProfile: userProfile
-    });
-});
-
-/**
- * Server Activation
- */
-
-app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
-});
-
+import TestJS from "./TestJs.js";
+import ConsoleLogIt from "./ConsoleLogIt.js";
+import getJSON from "./getJSON";
 
 
 TestJS();
@@ -149,14 +19,16 @@ getJSON('http://localhost:8000/api/v1/cities',
 
             generateTableHead(table, data);
             generateTable(table, dataRecords);
-            /*
-            let data = Object.keys(records.data[0]);
-            generateTable(table, records.data); // generate the table first
-            generateTableHead(table, data); // then the head
-             */
+
+            //let data = Object.keys(records.data[0]);
+            //generateTable(table, records.data); // generate the table first
+            //generateTableHead(table, data); // then the head
+
         }
 
     });
+
+
 ConsoleLogIt("this works  in the bundle");
 
 function generateTableHead(table, data) {
