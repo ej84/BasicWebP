ff6141271baa8fc2f54c132e804cabcf9d320fe9
ej84 <ej84@njit.edu>
2021-05-15 02:37:31 -0400

    step 5: finished auth.js code

--- auth.js
+++ auth.js
@@ -1,14 +1,11 @@
 const express = require('express');
-const app = express();
-const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
-const accessTokenSecret = 'youraccesstokensecret';
+const jwt = require('jsonwebtoken');
 
-app.use(bodyParser.json());
+const app = express();
 
-app.listen(3000, () => {
-    console.log('Authentication service started on port 3000');
-});
+const accessTokenSecret = 'somerandomaccesstoken';
+const refreshTokenSecret = 'somerandomstringforrefreshtoken';
 
 const users = [
     {
@@ -20,23 +17,66 @@ const users = [
         password: 'password123member',
         role: 'member'
     }
-];
+]
+
+let refreshTokens = [];
+
+app.use(bodyParser.json());
 
 app.post('/login', (req, res) => {
-    // Read username and password from request body
+    // read username and password from request body
     const { username, password } = req.body;
 
-    // Filter user from the users array by username and password
+    // filter user from the users array by username and password
     const user = users.find(u => { return u.username === username && u.password === password });
 
     if (user) {
-        // Generate an access token
-        const accessToken = jwt.sign({ username: user.username,  role: user.role }, accessTokenSecret);
+        // generate an access token
+        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+        const refreshToken = jwt.sign({ username: user.username, role: user.role }, refreshTokenSecret);
+
+        refreshTokens.push(refreshToken);
 
         res.json({
-            accessToken
+            accessToken,
+            refreshToken
         });
     } else {
         res.send('Username or password incorrect');
     }
+});
+
+app.post('/token', (req, res) => {
+    const { token } = req.body;
+
+    if (!token) {
+        return res.sendStatus(401);
+    }
+
+    if (!refreshTokens.includes(token)) {
+        return res.sendStatus(403);
+    }
+
+    jwt.verify(token, refreshTokenSecret, (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+
+        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+
+        res.json({
+            accessToken
+        });
+    });
+});
+
+app.post('/logout', (req, res) => {
+    const { token } = req.body;
+    refreshTokens = refreshTokens.filter(t => t !== token);
+
+    res.send("Logout successful");
+});
+
+app.listen(3000, () => {
+    console.log('Authentication service started on port 3000');
 });
