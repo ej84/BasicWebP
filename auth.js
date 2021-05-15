78cf9eb965bb55f37cf16bfe6d0968a193f57796
ej84 <ej84@njit.edu>
2021-05-15 01:57:30 -0400

    step 1: re-created auth.js for jwt token authentication.

--- auth.js
+++ auth.js
@@ -1,77 +1,42 @@
-// auth.js
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const accessTokenSecret = 'youraccesstokensecret';
 
-/**
- * Required External Modules
- */
+app.use(bodyParser.json());
 
-const express = require("express");
-const router = express.Router();
-const passport = require("passport");
-const querystring = require("querystring");
+app.listen(3000, () => {
+    console.log('Authentication service started on port 3000');
+});
 
-require("dotenv").config();
-
-/**
- * Routes Definitions
- */
-
-router.get(
-    "/login",
-    passport.authenticate("auth0", {
-        scope: "openid email profile"
-    }),
-    (req, res) => {
-        res.redirect("/");
+const users = [
+    {
+        username: 'john',
+        password: 'password123admin',
+        role: 'admin'
+    }, {
+        username: 'anna',
+        password: 'password123member',
+        role: 'member'
     }
-);
+];
 
-router.get("/callback", (req, res, next) => {
-    passport.authenticate("auth0", (err, user, info) => {
-        if (err) {
-            return next(err);
-        }
-        if (!user) {
-            return res.redirect("/login");
-        }
-        req.logIn(user, (err) => {
-            if (err) {
-                return next(err);
-            }
-            const returnTo = req.session.returnTo;
-            delete req.session.returnTo;
-            res.redirect(returnTo || "/");
+app.post('/login', (req, res) => {
+    // Read username and password from request body
+    const { username, password } = req.body;
+
+    // Filter user from the users array by username and password
+    const user = users.find(u => { return u.username === username && u.password === password });
+
+    if (user) {
+        // Generate an access token
+        const accessToken = jwt.sign({ username: user.username,  role: user.role }, accessTokenSecret);
+
+        res.json({
+            accessToken
         });
-    })(req, res, next);
-});
-
-router.get("/logout", (req, res) => {
-    req.logOut();
-
-    let returnTo = req.protocol + "://" + req.hostname;
-    const port = req.connection.localPort;
-
-    if (port !== undefined && port !== 80 && port !== 443) {
-        returnTo =
-            process.env.NODE_ENV === "production"
-                ? `${returnTo}/`
-                : `${returnTo}:${port}/`;
+    } else {
+        res.send('Username or password incorrect');
     }
-
-    const logoutURL = new URL(
-        `https://${process.env.AUTH0_DOMAIN}/v2/logout`
-    );
-
-    const searchString = querystring.stringify({
-        client_id: process.env.AUTH0_CLIENT_ID,
-        returnTo: returnTo
-    });
-    logoutURL.search = searchString;
-
-    res.redirect(logoutURL);
 });
-
-/**
- * Module Exports
- */
-
-module.exports = router;
